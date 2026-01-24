4ee26b5ec3917ebf76fb3dcec2da19cb9660fb8f
Renov30 <dalero33ermiawan@gmail.com>
2026-01-24 19:11:37 +0900

    feat(usermain): add simple layout for front ui

--- orderin-app/app/page.tsx
+++ orderin-app/app/page.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="bg-gray-300 flex justify-center items-center m-5">
+    <div className="bg-gray-300 flex flex-col justify-center items-center m-5">
       {/* pilihan jenis menu */}
-      <div className="flex p-5">
+      <div className="flex p-5 bg-amber-300">
         <button className="bg-gray-500 px-7 py-2 w-[200px] rounded-3xl m-2 flex flex-col items-center hover:bg-gray-600 transition ease-in-out cursor-pointer">
           <p>Makanan Berat</p>
         </button>
@@ -18,6 +18,23 @@ export default function Home() {
           <p>Menu Anak</p>
         </button>
       </div>
+      {/* tombol navigasi dan pengaturan */}
+      <div className="flex">
+        <div className="flex flex-col p-5 bg-amber-600">
+          <div className="p-5 bg-blue-400 mb-5 rounded-xl"></div>
+          <div className="p-5 bg-blue-500 mb-5 rounded-xl"></div>
+          <div className="p-5 bg-blue-600 mb-5 rounded-xl"></div>
+        </div>
+      {/* tampilan menu yang ada */}
+        <div className="flex p-5 bg-amber-700">
+          {/* card */}
+          <div className="p-5 bg-white mx-5"></div>
+          <div className="p-5 bg-white mx-5"></div>
+          <div className="p-5 bg-white mx-5"></div>
+          <div className="p-5 bg-white mx-5"></div>
+        </div>
+      </div>
+      
     </div>
   );
 }
