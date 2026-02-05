ba8f31cc9521a7b46ebfd0bc89668bcda45351b5
Renov30 <dalero33ermiawan@gmail.com>
2026-02-05 15:04:54 +0900

    feat(ui): tambah icon menggunakan heroicons

--- orderin-app/app/page.tsx
+++ orderin-app/app/page.tsx
@@ -1,32 +1,44 @@
 import Image from "next/image";
+import {ArrowDownIcon, ChevronDownIcon, ChevronUpIcon, Cog8ToothIcon, ShoppingCartIcon}  from "@heroicons/react/24/solid";
 
 export default function Home() {
   return (
     <div className="container mx-auto">
       <div className="min-h-screen bg-latar">
-        {/* pilihan jenis menu */}
-        <div className="flex flex-wrap justify-center gap-5 py-5">
-          <button className="bg-merah px-7 py-2 w-[200px] rounded-3xl m-2 flex flex-col items-center hover:bg-gray-600 transition ease-in-out cursor-pointer">
-            <p className="font-bold">Makanan Berat</p>
-          </button>
-          <button className="bg-merah px-7 py-2 w-[200px] rounded-3xl m-2 flex flex-col items-center hover:bg-gray-600 transition ease-in-out cursor-pointer">
-            <p className="font-bold">Minuman</p>
-          </button>
-          <button className="bg-merah px-7 py-2 w-[200px] rounded-3xl m-2 flex flex-col items-center hover:bg-gray-600 transition ease-in-out cursor-pointer">
-            <p className="font-bold">Snack</p>
-          </button>
-          <button className="bg-merah px-7 py-2 w-[200px] rounded-3xl m-2 flex flex-col items-center hover:bg-gray-600 transition ease-in-out cursor-pointer">
-            <p className="font-bold">Menu Anak</p>
-          </button>
-        </div>
-        <div className="flex">
+        <div className="flex h-screen">
           {/* tombol navigasi dan pengaturan */}
+          <div className="flex items-center">
             <div className="flex flex-col p-5 ">
-              <div className="p-5 bg-merah mb-5 rounded-xl"></div>
-              <div className="p-5 bg-merah mb-5 rounded-xl"></div>
-              <div className="p-5 bg-merah mb-5 rounded-xl"></div>
+              <div className="p-5 bg-merah mb-5 rounded-xl">
+                   <ChevronUpIcon className="h-6 w-6 text-white"/>
+              </div>
+              <div className="p-5 bg-merah mb-5 rounded-xl">
+                <Cog8ToothIcon className="h-6 w-6 text-white"/>
+              </div>
+              <div className="p-5 bg-merah mb-5 rounded-xl">
+                <ChevronDownIcon className="h-6 w-6 text-white"/>
+              </div>
             </div>    
-          {/* tampilan menu yang ada */}
+          </div>
+
+          {/* jenis menu dan menu */}
+          <div>
+            {/* pilihan jenis menu */}
+            <div className="flex flex-wrap justify-center gap-5 py-5">
+              <button className="bg-merah px-7 py-2 w-[200px] rounded-3xl m-2 flex flex-col items-center hover:bg-gray-600 transition ease-in-out cursor-pointer">
+                <p className="font-bold">Makanan Berat</p>
+              </button>
+              <button className="bg-merah px-7 py-2 w-[200px] rounded-3xl m-2 flex flex-col items-center hover:bg-gray-600 transition ease-in-out cursor-pointer">
+                <p className="font-bold">Minuman</p>
+              </button>
+              <button className="bg-merah px-7 py-2 w-[200px] rounded-3xl m-2 flex flex-col items-center hover:bg-gray-600 transition ease-in-out cursor-pointer">
+                <p className="font-bold">Snack</p>
+              </button>
+              <button className="bg-merah px-7 py-2 w-[200px] rounded-3xl m-2 flex flex-col items-center hover:bg-gray-600 transition ease-in-out cursor-pointer">
+                <p className="font-bold">Menu Anak</p>
+              </button>
+            </div>
+            {/* tampilan menu yang ada */}
             <div className="grid grid-cols-4 gap-4 px-5 py-5 w-full">
               {/* card */}
               <div className="bg-putih flex flex-col items-center rounded-xl">
@@ -38,13 +50,15 @@ export default function Home() {
                   <p className="text-ijo-gelap">Rp. 20.000,00</p>
                 </div>
                 <div className="p-2 flex justify-center items-center w-full ">
-                  <button className="bg-oranye px-4 py-2 rounded-2xl w-full hover:bg-amber-700 transition ease-in-out cursor-pointer font-bold">
+                  <button className="flex items-center justify-center bg-oranye px-4 py-2 rounded-2xl w-full hover:bg-amber-700 transition ease-in-out cursor-pointer text-xl font-bold">
+                  <ShoppingCartIcon className="h-6 w-6 text-white mr-2"/>
                   TAMBAH
                   </button>
                 </div>
               </div>
-            </div>
+            </div>  
           </div>
+        </div>
       </div>
     </div>
   );
