9b560e0428677d7983c6157f319e86b921fb1c64
Renov30 <dalero33ermiawan@gmail.com>
2026-02-07 15:41:58 +0900

    feat(ui): change padding and duplicate menu

--- orderin-app/app/page.tsx
+++ orderin-app/app/page.tsx
@@ -8,15 +8,15 @@ export default function Home() {
         <div className="flex h-screen">
           {/* tombol navigasi dan pengaturan */}
           <div className="flex items-center">
-            <div className="flex flex-col p-5 ">
-              <button className="p-5 bg-merah mb-5 rounded-xl hover:bg-red-900 transition ease-in-out cursor-pointer">
-                   <ChevronUpIcon className="h-6 w-6 text-white"/>
+            <div className="flex flex-col pl-5 ">
+              <button className="p-4 bg-merah mb-5 rounded-xl hover:bg-red-900 transition ease-in-out cursor-pointer">
+                   <ChevronUpIcon className="h-8 w-8 text-white"/>
               </button>
-              <div className="p-5 bg-merah mb-5 rounded-xl hover:bg-red-900 transition ease-in-out cursor-pointer">
-                <Cog8ToothIcon className="h-6 w-6 text-white"/>
+              <div className="p-4 bg-merah mb-5 rounded-xl hover:bg-red-900 transition ease-in-out cursor-pointer">
+                <Cog8ToothIcon className="h-8 w-8 text-white"/>
               </div>
-              <div className="p-5 bg-merah mb-5 rounded-xl hover:bg-red-900 transition ease-in-out cursor-pointer">
-                <ChevronDownIcon className="h-6 w-6 text-white"/>
+              <div className="p-4 bg-merah mb-5 rounded-xl hover:bg-red-900 transition ease-in-out cursor-pointer">
+                <ChevronDownIcon className="h-8 w-8 text-white"/>
               </div>
             </div>    
           </div>
@@ -39,14 +39,119 @@ export default function Home() {
               </button>
             </div>
             {/* tampilan menu yang ada */}
-            <div className="grid grid-cols-4 gap-4 px-5 py-5 w-full">
+            <div className="grid grid-cols-4 gap-5 px-10 py-5 w-full">
               {/* card */}
               <div className="bg-putih flex flex-col items-center rounded-xl">
                 <div className="w-full bg-abu-skeleton rounded-t-xl overflow-hidden">
                   <Image src="/images/menus/chicken-pizza.webp" alt="nama menu" width={500} height={200}/> 
                 </div>
+                <div className="px-2 pt-2 flex flex-col w-full">
+                  <p className="text-item font-bold text-2xl">Pizza Peperoni</p>
+                  <p className="text-ijo-gelap">Rp. 20.000,00</p>
+                </div>
+                <div className="p-2 flex justify-center items-center w-full ">
+                  <button className="flex items-center justify-center bg-oranye px-4 py-2 rounded-2xl w-full hover:bg-amber-600 transition ease-in-out cursor-pointer text-xl font-bold">
+                  <ShoppingCartIcon className="h-6 w-6 text-white mr-2"/>
+                  TAMBAH
+                  </button>
+                </div>
+              </div>
+              <div className="bg-putih flex flex-col items-center rounded-xl">
+                <div className="w-full bg-abu-skeleton rounded-t-xl overflow-hidden">
+                  <Image src="/images/menus/chicken-pizza.webp" alt="nama menu" width={500} height={200}/> 
+                </div>
                 <div className="p-2 flex flex-col w-full">
-                  <p className="text-item font-bold text-xl">Pizza Peperoni</p>
+                  <p className="text-item font-bold text-2xl">Pizza Peperoni</p>
+                  <p className="text-ijo-gelap">Rp. 20.000,00</p>
+                </div>
+                <div className="p-2 flex justify-center items-center w-full ">
+                  <button className="flex items-center justify-center bg-oranye px-4 py-2 rounded-2xl w-full hover:bg-amber-600 transition ease-in-out cursor-pointer text-xl font-bold">
+                  <ShoppingCartIcon className="h-6 w-6 text-white mr-2"/>
+                  TAMBAH
+                  </button>
+                </div>
+              </div>
+              <div className="bg-putih flex flex-col items-center rounded-xl">
+                <div className="w-full bg-abu-skeleton rounded-t-xl overflow-hidden">
+                  <Image src="/images/menus/chicken-pizza.webp" alt="nama menu" width={500} height={200}/> 
+                </div>
+                <div className="p-2 flex flex-col w-full">
+                  <p className="text-item font-bold text-2xl">Pizza Peperoni</p>
+                  <p className="text-ijo-gelap">Rp. 20.000,00</p>
+                </div>
+                <div className="p-2 flex justify-center items-center w-full ">
+                  <button className="flex items-center justify-center bg-oranye px-4 py-2 rounded-2xl w-full hover:bg-amber-600 transition ease-in-out cursor-pointer text-xl font-bold">
+                  <ShoppingCartIcon className="h-6 w-6 text-white mr-2"/>
+                  TAMBAH
+                  </button>
+                </div>
+              </div>
+              <div className="bg-putih flex flex-col items-center rounded-xl">
+                <div className="w-full bg-abu-skeleton rounded-t-xl overflow-hidden">
+                  <Image src="/images/menus/chicken-pizza.webp" alt="nama menu" width={500} height={200}/> 
+                </div>
+                <div className="p-2 flex flex-col w-full">
+                  <p className="text-item font-bold text-2xl">Pizza Peperoni</p>
+                  <p className="text-ijo-gelap">Rp. 20.000,00</p>
+                </div>
+                <div className="p-2 flex justify-center items-center w-full ">
+                  <button className="flex items-center justify-center bg-oranye px-4 py-2 rounded-2xl w-full hover:bg-amber-600 transition ease-in-out cursor-pointer text-xl font-bold">
+                  <ShoppingCartIcon className="h-6 w-6 text-white mr-2"/>
+                  TAMBAH
+                  </button>
+                </div>
+              </div>
+              <div className="bg-putih flex flex-col items-center rounded-xl">
+                <div className="w-full bg-abu-skeleton rounded-t-xl overflow-hidden">
+                  <Image src="/images/menus/chicken-pizza.webp" alt="nama menu" width={500} height={200}/> 
+                </div>
+                <div className="p-2 flex flex-col w-full">
+                  <p className="text-item font-bold text-2xl">Pizza Peperoni</p>
+                  <p className="text-ijo-gelap">Rp. 20.000,00</p>
+                </div>
+                <div className="p-2 flex justify-center items-center w-full ">
+                  <button className="flex items-center justify-center bg-oranye px-4 py-2 rounded-2xl w-full hover:bg-amber-600 transition ease-in-out cursor-pointer text-xl font-bold">
+                  <ShoppingCartIcon className="h-6 w-6 text-white mr-2"/>
+                  TAMBAH
+                  </button>
+                </div>
+              </div>
+              <div className="bg-putih flex flex-col items-center rounded-xl">
+                <div className="w-full bg-abu-skeleton rounded-t-xl overflow-hidden">
+                  <Image src="/images/menus/chicken-pizza.webp" alt="nama menu" width={500} height={200}/> 
+                </div>
+                <div className="p-2 flex flex-col w-full">
+                  <p className="text-item font-bold text-2xl">Pizza Peperoni</p>
+                  <p className="text-ijo-gelap">Rp. 20.000,00</p>
+                </div>
+                <div className="p-2 flex justify-center items-center w-full ">
+                  <button className="flex items-center justify-center bg-oranye px-4 py-2 rounded-2xl w-full hover:bg-amber-600 transition ease-in-out cursor-pointer text-xl font-bold">
+                  <ShoppingCartIcon className="h-6 w-6 text-white mr-2"/>
+                  TAMBAH
+                  </button>
+                </div>
+              </div>
+              <div className="bg-putih flex flex-col items-center rounded-xl">
+                <div className="w-full bg-abu-skeleton rounded-t-xl overflow-hidden">
+                  <Image src="/images/menus/chicken-pizza.webp" alt="nama menu" width={500} height={200}/> 
+                </div>
+                <div className="p-2 flex flex-col w-full">
+                  <p className="text-item font-bold text-2xl">Pizza Peperoni</p>
+                  <p className="text-ijo-gelap">Rp. 20.000,00</p>
+                </div>
+                <div className="p-2 flex justify-center items-center w-full ">
+                  <button className="flex items-center justify-center bg-oranye px-4 py-2 rounded-2xl w-full hover:bg-amber-600 transition ease-in-out cursor-pointer text-xl font-bold">
+                  <ShoppingCartIcon className="h-6 w-6 text-white mr-2"/>
+                  TAMBAH
+                  </button>
+                </div>
+              </div>
+              <div className="bg-putih flex flex-col items-center rounded-xl">
+                <div className="w-full bg-abu-skeleton rounded-t-xl overflow-hidden">
+                  <Image src="/images/menus/chicken-pizza.webp" alt="nama menu" width={500} height={200}/> 
+                </div>
+                <div className="p-2 flex flex-col w-full">
+                  <p className="text-item font-bold text-2xl">Pizza Peperoni</p>
                   <p className="text-ijo-gelap">Rp. 20.000,00</p>
                 </div>
                 <div className="p-2 flex justify-center items-center w-full ">
